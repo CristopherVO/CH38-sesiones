910bf5558cfc1abdf56ea627392dc3f59a660d0a
Cristopher Valencia <cristopherozzy17@gmail.com>
2024-02-19 13:26:05 -0600

    descripcion del ejercicio

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hola ch38");
+
+function enviarSaludo(){
+    alert('Hola ch38!!!!!!!!');
+}
+
+/**
+ * Pedir el nombre de la persona usando un promt()
+ * Mostrar un saludo y nombre de la persona con alert
+ */
+
+function enviarSaludoAPersona(){
+
+    let nombre=prompt();
+    alert(`Hola ${nombre}`);
+}
+
+//funcion que envie un saludo con alert.
